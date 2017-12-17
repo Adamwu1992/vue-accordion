7ee4f345df6e3ea232af72d7567c71cd295e3008
adam <wuyaf@servyou.cn>
2017-12-17 19:10:01 +0800

    init event emitter

--- src/accordion-menu/index.js
+++ src/accordion-menu/index.js
@@ -1,13 +1,9 @@
 import AccordionMenu from './src/accordion-menu.vue';
-import EventBroadcast from './src/util/vue.broadcast.js';
-import EventDispatch from './src/util/vue.dispatch.js';
 import './src/style.scss';
 import './src/font/iconfont.css';
 
 AccordionMenu.install = Vue => {
     Vue.component(AccordionMenu.name, AccordionMenu);
-    Vue.use(EventBroadcast);
-    Vue.use(EventDispatch);
 };
 
 export default AccordionMenu;
