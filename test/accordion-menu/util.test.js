7dc23ceb0bc20ed9daace56e77299e6e406c8063
adam <adamwu1992@163.com>
2017-12-24 16:32:46 +0800

    add test

--- test/accordion-menu/util.test.js
+++ test/accordion-menu/util.test.js
@@ -6,8 +6,53 @@ const should = chai.should();
 describe('Util', function() {
     //
     describe('traverse', function() {
-        it('should return 1', function() {
-            'a'.should.be.a('string')
+        const tree = [
+            {
+                id: 1,
+                name: 'mocha',
+                children: [
+                    {
+                        id: 11,
+                        name: 'hello',
+                        children: [
+                            {
+                                id: 112,
+                                name: 'chai'
+                            },
+                            {
+                                id: 119,
+                                name: 'latte'
+                            }
+                        ]
+                    },
+                    {
+                        id: 12,
+                        name: 'node'
+                    }
+                ]
+            },
+            {
+                id: 2,
+                name: 'good'
+            },
+            {
+                id: 3,
+                name: 'sun',
+                children: [
+                    {
+                        id: 31,
+                        name: 'moon'
+                    }
+                ]
+            }
+        ];
+        it('深度优先遍历所有节点', function() {
+            const excepted = 'mochahellochailattenodegoodsunmoon';
+            let s = '';
+            traverse(tree, node => {
+                s += node.name;
+            })
+            s.should.be.equal(excepted);
         })
     })
 })
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const should = chai.should();
 
-describe('it is a test', function() {
+describe('test index', function() {
     it('should return true', function() {
         '1'.should.be.a('string');
     })
